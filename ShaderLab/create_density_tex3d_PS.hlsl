910b851b6aab839428ece14f3414ba06617ca656
Bernhard Rieder <bernhard.rieder@live.at>
2017-04-13 00:29:20 +0200

    tweaked input vars and added pillars;

--- ShaderLab/create_density_tex3d_PS.hlsl
+++ ShaderLab/create_density_tex3d_PS.hlsl
@@ -6,28 +6,26 @@ struct g2pConnector
     float4 rsCoord : SV_POSITION;
 };
 
-
 float main(g2pConnector input) : SV_TARGET
 {    
-    float density = 1.f;
-    float distToCenter = distance(input.pos.xy, float2(0.f, 0.f));
-    /** filled circle */
-    if (distToCenter <= 0.1f)
-        density = 0.f;
+    float density = 0.f;
+    //float distToCenter = distance(input.pos.xy, float2(0.f, 0.f));
+    ///** filled circle */
+    //if (distToCenter <= 0.1f)
+    //    density = 0.f;
 
-    /** circle */
-    if (distToCenter <= 0.9f && distToCenter >= 0.85f)
-        density = 0.f;
+    ///** circle */
+    //if (distToCenter <= 0.9f && distToCenter >= 0.85f)
+    //    density = 0.f;
 
-    //if (input.index == 0)
-    //    return 0.1f;
-    //else
-    //    return 0.5f;
-
-    density = 0.f;
+    //tweak input
+    input.pos.xy *= 2.0f; // xy scaling
+    input.pos.z *= 0.5f;
 
     //pillar (slide 12)
     density += 1 / length(input.pos.xy - float2(-0.2f, 0.5f)) - 1;
+    density += 1 / length(input.pos.xy - float2(1.f, 0.9f)) - 1;
+    density += 1 / length(input.pos.xy - float2(-0.6f, -.5f)) - 1;
     
     //water flow channel (slide 14)
     density -= 1 / length(input.pos.xy) - 1;
@@ -41,5 +39,6 @@ float main(g2pConnector input) : SV_TARGET
 
     //shelves (slide 17)
     density += cos(input.pos.z);
+
     return density;
 }
