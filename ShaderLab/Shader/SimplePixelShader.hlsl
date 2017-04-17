d53b7746393687f4f72a889041f6cc74bec5b608
Bernhard Rieder <bernhard.rieder@live.at>
2017-04-17 15:38:46 +0200

    code cleanup

--- ShaderLab/Shader/SimplePixelShader.hlsl
+++ ShaderLab/Shader/SimplePixelShader.hlsl
@@ -1,15 +1,11 @@
-struct g2pConnector
+struct v2pConnector
 {
     float4 pos : SV_Position;
     float4 color : COLOR;
     float3 wsNormal : NORMAL;
 };
 
-
-float4 main(g2pConnector IN) : SV_Target
+float4 main(v2pConnector IN) : SV_Target
 {
-    //IN.color.xyz = 0.f;
-    
-    //return IN.color;
     return float4(1.f, 0.f, 0.f, 1.0f);
 }
--- ShaderLab/Shader/SimpleVertexShader.hlsl
+++ ShaderLab/Shader/SimpleVertexShader.hlsl
@@ -1,36 +1,37 @@
-#include "ShaderLab.hlsli"
-
 struct a2vConnector
 {
     float4 wsCoord_Ambo : POSITION;
     float3 wsNormal : NORMAL;
 };
 
-struct v2gConnector
-{
-    float4 color : COLOR;
-    float4 pos : POSITION1;
-    float4 originalPos : POSITION;
-};
-
-struct g2pConnector
+struct v2pConnector
 {
     float4 pos : SV_Position;
     float4 color : COLOR;
     float3 wsNormal : NORMAL;
 };
 
-g2pConnector main(a2vConnector IN)
+cbuffer PerApplication : register(b0)
 {
-    g2pConnector OUT;
+    matrix projectionMatrix;
+}
+
+cbuffer PerFrame : register(b1)
+{
+    matrix viewMatrix;
+}
+
+cbuffer PerObject : register(b2)
+{
+    matrix worldMatrix;
+}
+
+v2pConnector main(a2vConnector IN)
+{
+    v2pConnector OUT;
 
     matrix mvp = mul(projectionMatrix, mul(viewMatrix, worldMatrix));
     OUT.pos = mul(mvp, float4(IN.wsCoord_Ambo.xyz, 1.f));
-    //OUT.pos = float4(IN.wsCoord_Ambo.xyz, 0);
-    //OUT.originalPos = IN.wsCoord_Ambo;
-    //OUT.color = float4(IN.color, 1.0f);
-
-    //OUT.pos = IN.wsCoord_Ambo;
     OUT.color = float4(1.f, 0.f, 0.f, 1.0f);
     OUT.wsNormal = IN.wsNormal;
 
